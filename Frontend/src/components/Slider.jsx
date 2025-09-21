6369b99c9cdb1ac122bab14fa52ab54a4cb56aaf
vanshg0802278@gmail.com <vanshg0802278@gmail.com>
2025-09-21 11:15:02 +0530

    add

--- Frontend/src/components/Slider.jsx
+++ Frontend/src/components/Slider.jsx
@@ -4,8 +4,27 @@ function Slider() {
     <>
       <div className={styles.container}>
         <div className={styles.container2}>
-        <div>col1</div>
-        <div>col1</div>
+          <div className={styles.container3}>
+            {/* col1 */}
+            <h1>
+              Find the Best <br /> Eduction Loan <br /> for Your Future
+            </h1>
+            <p>
+              Compare loan options from multiple lenders, get personalized
+              recommendations, and make informed decisions about your education
+              financing with our smart platform.
+            </p>
+
+            <div className={styles.searchBox}>
+                <h4>Quick Loan Estimate</h4>
+            </div>
+            
+            <div className={styles.buttonBox}>
+                <button>Start Comparison</button>
+                <button>Learn More</button>
+            </div>
+          </div>
+          <div className={styles.container3} >col1</div>
         </div>
       </div>
     </>
